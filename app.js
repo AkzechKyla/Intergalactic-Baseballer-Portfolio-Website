069503c21611b6255f58bcbbb20849af7caf1e7a
Kyla <kflmorcillos@gmail.com>
2024-07-16 13:42:23 +0800

    Refactor pageTransitions()

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ function pageTransitions() {
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function() {
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
+            currentBtn[0].classList.remove('active-btn');
+            this.classList.add('active-btn');
         })
     }
 }
